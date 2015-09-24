b61f1b4dcc7f7f16f3bc0d9aaf17055569086c37
finetype <finesttype@gmail.com>
2015-09-24 17:08:38 -0500

    some initial structure

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -2,8 +2,13 @@
 		// Auth  = require ('./auth.js');
 
 module.exports = {
+	gameCodes: [],
 	doSomething: function(req, res){
 		console.log("hi! ");
 		res.send("howdy!");
+	},
+	gameMaker: function(req, res){
+		//should take in username, something like req.headers.username
+
 	}
 };

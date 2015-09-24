58f8ad52f54dbcde087ec737438179172682d94a
finetype <finesttype@gmail.com>
2015-09-24 15:33:24 -0500

    first iteration of a server. runs, returns 'howdy' on receiving a GET request to /test. has a request handler file it imports. has some middleware. be sure to run npm install, as express and the middleware need to be installed to run the server. (everyone will have to do that after this merge.)

---
+++ server/requestHandler.js
@@ -0,0 +1,9 @@
+// var db = require('../models/userData.js'),
+		// Auth  = require ('./auth.js');
+
+module.exports = {
+	doSomething: function(req, res){
+		console.log("hi! ");
+		res.send("howdy!")
+	}
+};

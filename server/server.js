9f6f05707edd3a3de1e96a8fa73fe12e8587c3cb
juandsierra82 <juandsierra82@gmail.com>
2015-09-24 15:35:26 -0500

    server testing

---
+++ server/server.js
@@ -0,0 +1,18 @@
+//Dummy server for testing the test file purposes
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+
+// app.post()
+
+// });
+
+// module.exports = app;
+// // var server = app.listen(3000, function () {
+// //   var host = server.address().address;
+// //   var port = server.address().port;
+
+// //   console.log('Example app listening at http://%s:%s', host, port);
+// // });

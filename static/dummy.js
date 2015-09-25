0defeb1d33cc7041f8add01bbfb3959133fdd46f
juandsierra82 <juandsierra82@gmail.com>
2015-09-25 12:02:46 -0500

    merge conflicts

--- static/dummy.js
+++ static/dummy.js
@@ -3,11 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('studentView.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+//server side socket connection will need updating for routers: event name is 'buzz'
+io.on('connection', function (socket){
+ socket.on('buzz', function (data){
+ 	console.log('this is the server data: '+data.toLowerCase());
+  io.emit('buzz', data.toLowerCase());
+	});
 });
 
 http.listen(3000, function(){

b542bb44bd97c437fe5530b7c34504c8909a540d
Brian Boyko <brian.boyko@gmail.com>
2015-09-25 16:53:31 -0500

    bb-dev/studentview-websockets

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var bodyParser  = require('body-parser');
 var express     = require('express');
-var	morgan      = require('morgan');
-var	handler     = require('./server/requestHandler.js');
-
 var app = express();
-var PORT = 8000;
+var http = require('http');
+// var server = require('http').createServer(app);
+var morgan      = require('morgan');
+var handler     = require('./server/requestHandler.js');
+
+var PORT = process.env.PORT || 8000;
+
+
+
+
 
 app.use(bodyParser.json());
 app.use('/client',express.static(__dirname + '/client'));
-app.use('/css',express.static(__dirname + '/client/css'));
-app.use('/js',express.static(__dirname + '/client/js'));
 // app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(morgan('dev'));
 
@@ -19,6 +23,28 @@ app.get('/test', function(req, res) {
 );
 
 console.log('App is listening on port ' + PORT);
-app.listen(PORT);
+var server = app.listen(PORT);
+var io = require('socket.io').listen(server)
 
 module.exports = app;
+
+//--------------------------------
+// WEBSOCKETS -- Everything that requires Websockets lives INSIDE this callback.
+//--------------------------------
+
+//Everything that requires Websockets lives INSIDE this callback.
+io.sockets.on('connection', function(socket) {
+
+  socket.on('buzz', function(studentBuzzer) {
+    console.log("ServerSide Student: ", studentBuzzer.name, "Room: ", studentBuzzer.room, "Timestamp:", studentBuzzer.timestamp)
+    io.emit('buzzResponse', ('buzzResponse recieved from server after ' + studentBuzzer.name + ' hit the buzzer at ' + studentBuzzer.timestamp + ' in ' + studentBuzzer.room))
+  }); // end chat message
+
+
+
+});
+
+//--------------------------------
+
+// END WEBSOCKETS
+//--------------------------------

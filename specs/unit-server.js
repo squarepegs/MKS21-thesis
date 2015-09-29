de581ca27eae5d1ca79c126f49accab573ff6b83
juandsierra82 <juandsierra82@gmail.com>
2015-09-29 18:55:48 -0500

    [socket] message testing to all users

--- specs/unit-server.js
+++ specs/unit-server.js
@@ -18,6 +18,25 @@ var student3 = {'name':'Charlie'};
 
 describe('Basic server', function(){
 
+//test for broadcast of one user: one logs in, server emits a new user connection.
+
+  it('Should broadcast new user once they connect',function(done){
+    var client = io.connect(socketURL, options);
+
+    client.on('connect',function(data){
+      client.emit('connection name', student1);
+    });
+
+    client.on('new user',function(usersName){
+      usersName.should.be.type('string');
+      usersName.should.equal(student1.name + " has joined");
+      client.disconnect();
+      done(); 
+    });
+  });
+
+	//test for two users: one user logs in, second user emits login and first user waits for second user to emit new log in. 
+
 	it('Should broadcast new user to all users', function(done){
 		
 		var numUsers = 0;
@@ -50,4 +69,41 @@ describe('Basic server', function(){
 	    });
 	  });	  
 	});
+
+	  it('Should be able to broadcast messages', function (done){
+    var client1, client2, client3;
+    var message = 'Hello World';
+    var messages = 0;
+
+    var checkMessage = function(client){
+      client.on('message', function (msg){
+        message.should.equal(msg);
+        client.disconnect();
+        messages++;
+        if(messages === 3){
+        messages.should.equal(3);  
+          done();
+        };
+      });
+    };
+
+    client1 = io.connect(socketURL, options);
+    checkMessage(client1);
+
+    client1.on('connect', function(data){
+      client2 = io.connect(socketURL, options);
+      checkMessage(client2);
+
+      client2.on('connect', function(data){
+        client3 = io.connect(socketURL, options);
+        checkMessage(client3);
+
+        client3.on('connect', function(data){
+          client2.send(message);
+        });
+      });
+    });
+  });
+
+
 });

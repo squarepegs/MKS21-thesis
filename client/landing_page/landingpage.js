ef410d60c68ab02fd148182bf83d1be5ceb169f2
Brian Boyko <brian.boyko@gmail.com>
2015-10-04 20:00:29 -0500

    Ready for working with team

--- client/landing_page/landingpage.js
+++ client/landing_page/landingpage.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
   $('.modal-trigger').leanModal();
 });
 
+var signUp = function(username, password) {
+  //TODO --- somehow get an HTTP request to http:// URL / signup / username /password
+}
+
+$('#createLoginButton').on('click', function() {
+  var u = $('#signupUsername').val();
+  var p = $('#signupPassword').val();
+  signUp(u, p);
+});

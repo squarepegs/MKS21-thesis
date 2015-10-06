d7b7ef42a9681ab14eb82635cf53256ced66fa7b
Pete Do <sumofeleven@gmail.com>
2015-10-06 17:03:40 -0500

    added passport facebook new strategy

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 //TODO: need to make a user 
 var User = require('../db/models/userModel');
+var configAuth = require('./auth')
 
 module.exports = function(passport){
   
@@ -69,6 +70,36 @@ passport.use('local-signup', new LocalStrategy({
     });
   }
   ));
+
+  passport.use(new FacebookStrategy({
+    clientID: configAuth.facebookAuth.clientID,
+    clientSecret: configAuth.facebookAuth.clientSecret,
+    callbackURL: configAuth.facebookAuth.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function(){
+      User.findOne({'facebook.id': profile.id}, function(err, user){
+        if(err)
+          return done(err);
+        if(user)
+          return done(null,user);
+      }else{
+        var newUser = new User();
+        newUser.facebook.id = profile.id;
+        newUser.facebook.token = accessToken;
+        newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+        newUser.facebook.email = profile.emails[0].value;
+
+        newUser.save(function(err){
+          if(err)
+            throw err;
+
+          return done(null, newUser);
+        })
+      });
+    });
+  }
+));
    
 
 };

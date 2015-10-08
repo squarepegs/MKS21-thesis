69f170f45478d6cfce807a4daf89426996cc7bfa
Pete Do <sumofeleven@gmail.com>
2015-10-08 16:24:49 -0500

    3 charts rendering on dummy html page

--- client/teacher/charts.js
+++ client/teacher/charts.js
@@ -1,11 +1,62 @@
-var Chart = require('Chartjs');
+$( document ).ready(function() {
+	var ctx = document.getElementById("lineChart").getContext("2d");
+  var text = document.getElementById("barChart").getContext("2d");
+  var con = document.getElementById("radarChart").getContext("2d");
 
-var ctx = document.getElementById("lineChart").getContext("2d");
+	var data = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            fillColor: "rgba(220,220,220,0.2)",
+	            strokeColor: "rgba(220,220,220,1)",
+	            pointColor: "rgba(220,220,220,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(220,220,220,1)",
+	            data: [65, 59, 80, 81, 56, 55, 40]
+	        },
+	        {
+	            label: "My Second dataset",
+	            fillColor: "rgba(151,187,205,0.2)",
+	            strokeColor: "rgba(151,187,205,1)",
+	            pointColor: "rgba(151,187,205,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(151,187,205,1)",
+	            data: [28, 48, 40, 19, 86, 27, 90]
+	        }
+	    ]
+	};
 
-var myLineChart = new Chart(ctx).Line(data, options);
+	var data = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            fillColor: "rgba(220,220,220,0.2)",
+	            strokeColor: "rgba(220,220,220,1)",
+	            pointColor: "rgba(220,220,220,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(220,220,220,1)",
+	            data: [65, 59, 80, 81, 56, 55, 40]
+	        },
+	        {
+	            label: "My Second dataset",
+	            fillColor: "rgba(151,187,205,0.2)",
+	            strokeColor: "rgba(151,187,205,1)",
+	            pointColor: "rgba(151,187,205,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(151,187,205,1)",
+	            data: [28, 48, 40, 19, 86, 27, 90]
+	        }
+	    ]
+	};
 
-var data = {
-	labels: ["January", "February", "March", "April", "May", "June", "July"],
+	var data = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
     datasets: [
         {
             label: "My First dataset",
@@ -15,7 +66,7 @@ var data = {
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: [65, 59, 90, 81, 56, 55, 40]
         },
         {
             label: "My Second dataset",
@@ -25,8 +76,12 @@ var data = {
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            data: [28, 48, 40, 19, 96, 27, 100]
         }
     ]
 };
 
+	var myLineChart = new Chart(ctx).Line(data);
+	var mybarChart = new Chart(text).Bar(data);
+	var mypieChart = new Chart(con).Radar(data);
+});

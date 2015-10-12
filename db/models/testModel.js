ec84ed6335bdc8292fcada74a8f7caa8b0a69f78
Brian Boyko <brian.boyko@gmail.com>
2015-10-12 17:58:35 -0500

    Working on routing

---
+++ db/models/testModel.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var testSchema = mongoose.Schema({
+  _owner   : {type: String},
+  _deck    : {type: String},
+  time     : {type: Date}, 
+  questions: {
+    type: Array
+  }
+});
+
+module.exports = mongoose.model('deck', deckSchema);

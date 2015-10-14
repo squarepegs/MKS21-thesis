62c186cb8e48e6e2e1ff9747968c0b1bb9dab82b
Pete Do <sumofeleven@gmail.com>
2015-10-13 19:30:59 -0500

    browserifyed up

--- client/teacher/charts.js
+++ client/teacher/charts.js
@@ -41,32 +41,7 @@ $( document ).ready(function() {
 	    ]
 	};
 
-	var time = {
-		labels: ["January", "February", "March", "April", "May", "June", "July"],
-	    datasets: [
-	        {
-	            label: "Aggregate Attempted",
-	            fillColor: "rgba(220,220,220,0.2)",
-	            strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 80, 81, 56, 55, 40]
-	        },
-	        {
-	            label: "Aggregate Correct",
-	            fillColor: "rgba(151,187,205,0.2)",
-	            strokeColor: "rgba(151,187,205,1)",
-	            pointColor: "rgba(151,187,205,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(151,187,205,1)",
-	            data: [28, 48, 40, 19, 36, 37, 40]
-	        }
-	    ]
-	};
-
+	
 
 
 	var myLineChart = new Chart(ctx).Line(time);
--- client/teacher/dashboard.js
+++ client/teacher/dashboard.js
@@ -2,6 +2,88 @@
 var socket = io();
 window.jeopardy = {};
 
+var drawChart = function(){
+      var ctx = document.getElementById("lineChart").getContext("2d");
+      
+      var data = {
+        labels: ["John", "Fred", "Mary", "April", "David", "Jack", "Tomas"],
+          datasets: [
+              {
+                  label: "Total Questions",
+                  fillColor: "rgba(220,220,220,0.6)",
+                  strokeColor: "rgba(220,220,220,1)",
+                  pointColor: "rgba(220,220,220,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(220,220,220,1)",
+                  data: [20, 20, 20, 20, 20, 20, 20]
+              },
+
+              {
+                  label: "Questions Attempted",
+                  fillColor: "rgba(220,220,220,0.2)",
+                  strokeColor: "rgba(220,220,220,1)",
+                  pointColor: "rgba(220,220,220,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(220,220,220,1)",
+                  data: [12, 19, 10, 14, 8, 9, 18]
+              },
+
+              {
+                  label: "Questions Correct",
+                  fillColor: "rgba(151,187,205,0.2)",
+                  strokeColor: "rgba(151,187,205,1)",
+                  pointColor: "rgba(151,187,205,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(151,187,205,1)",
+                  data: [10, 15, 5, 9, 6, 7, 10]
+              }
+          ]
+      };
+
+      var time = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+              {
+                  label: "Aggregate Attempted",
+                  fillColor: "rgba(220,220,220,0.2)",
+                  strokeColor: "rgba(220,220,220,1)",
+                  pointColor: "rgba(220,220,220,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(220,220,220,1)",
+                  data: [65, 59, 80, 81, 56, 55, 40]
+              },
+              {
+                  label: "Aggregate Correct",
+                  fillColor: "rgba(151,187,205,0.2)",
+                  strokeColor: "rgba(151,187,205,1)",
+                  pointColor: "rgba(151,187,205,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(151,187,205,1)",
+                  data: [28, 48, 40, 19, 36, 37, 40]
+              }
+          ]
+      };
+
+
+
+      // var myLineChart = new Chart(ctx).Line(time);
+      // var mybarChart = new Chart(text).Bar(data);
+
+}
+
+    var MyLineChart = require('react-chartjs').Line;
+    
+    var LineChartComponent = React.createClass({
+      render: function(){
+        return (<MyLineChart data={this.state.chartData} options={this.state.chartOptions} width="600" height="400">)
+      }
+    })
+
 socket.on('error', function(){
   alert("there was a server error. Please try starting a new session.");
 });
@@ -35,7 +117,7 @@ var Tabs = React.createClass({
    classData:function(){
     console.log('classData')
     React.render(
-    <Charts />, document.getElementById('view'))
+    <Graph />, document.getElementById('view'))
   },
    studentData:function(){
     console.log('studentData')
@@ -66,6 +148,23 @@ var Tabs = React.createClass({
   }
 })
 
+var Graph = React.createClass({
+  componentDidMount: function(){
+    // drawChart();
+  },
+
+  render: function(){
+    return (
+      <canvas id="lineChart" width="600" height="400">Foo</canvas>
+      
+      )
+  }
+  
+
+})
+
+
+
 var ViewArea = React.createClass({
 
   render: function(){
@@ -73,14 +172,7 @@ var ViewArea = React.createClass({
   }
 })
 
-var Charts = React.createClass({
-  render: function(){
-    return (<div id="charts">
-      <canvas id="lineChart" width="600" height="400"></canvas>
-      <canvas id="barChart" width="600" height="400"></canvas>
-      </div>)
-  }
-})
+
 
 var Profile = React.createClass({
   getInitialState: function(){

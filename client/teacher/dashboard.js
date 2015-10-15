5d806fe331f330934e2c9acb4eb2aab5c84dcb55
Pete Do <sumofeleven@gmail.com>
2015-10-15 12:17:56 -0500

    added materialize to teacher dash board, will be changing branch

--- client/teacher/dashboard.js
+++ client/teacher/dashboard.js
@@ -570,12 +570,13 @@ var CreateDecks = React.createClass({
   }
 })
 
-
+React.render(
+  <Tabs />,document.getElementById('navbar')
+);
 // initial page render
 React.render(
   <div>
-    <Tabs />
-    <hr/><hr/>
+    
     <ViewArea />
 
   </div>,

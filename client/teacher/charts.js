17788872354ca699240f363df88f0169dbb0484e
Brian Boyko <brian.boyko@gmail.com>
2015-10-15 20:10:57 -0500

    dashboard and teacher styling

--- client/teacher/charts.js
+++ client/teacher/charts.js
@@ -1,4 +1,45 @@
+$(document).ready(function() {
+  getChartData();
+  var text = document.getElementById("barChart").getContext("2d");
+  var data = {
+    labels: ["John", "Fred", "Mary", "April", "David", "Jack", "Tomas"],
+    datasets: [
+      {
+        label: "Total Questions",
+        fillColor: "rgba(220,220,220,0.6)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [20, 20, 20, 20, 20, 20, 20]
+         },
 
+      {
+        label: "Questions Attempted",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [12, 19, 10, 14, 8, 9, 18]
+         },
+
+      {
+        label: "Questions Correct",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [10, 15, 5, 9, 6, 7, 10]
+         }
+     ]
+  };
+  // var mybarChart = new Chart(text).Bar(data);
+});
 var getParticipants = function(test) {
   console.log(test);
   output = {};
@@ -13,76 +54,37 @@ var getParticipants = function(test) {
   }
   return retOutput;
 }
-var feedbackByCategory = function(testData) {
-  var categoryBucket = {}
-  for (var q = 0; q < testData.length; q++) { // q = question
-    if (categoryBucket[testData[q].category] === undefined) {
-      categoryBucket[testData[q].category] = {};
-    }
-    for (var player in testData[q].feedbacks) {
-      if (categoryBucket[testData[q].category][player] === undefined) {
-        categoryBucket[testData[q].category][player] = [testData[q].feedbacks[player]]
-      } else {
-        if (testData[q].feedbacks[player]) {
-          categoryBucket[testData[q].category][player].push(testData[q].feedbacks[player])
-        }
-      }
-    }
-  }
-  return categoryBucket;
-}
 var getChartData = function() {
   $.get('/api/getTestData', function(req, res) {
     var text = document.getElementById("barChart").getContext("2d");
     console.log("tests req", req);
     testData = req[0].testData.slice(1);
-    // average score per category for player
-    var byCategory = feedbackByCategory(testData);
-    console.log(byCategory);
-    var displayData = {};
-    displayData.datasets = [];
-    displayData.labels = [];
-    playerAverages = {};
-    for (var category in byCategory) {
-      playerAverages[category] = [];
-      displayData.labels.push(category);
-      for (var player in byCategory[category]) {
-        console.log("byCategory[category][player]", byCategory[category][player], "category", category, "player", player)
-        var avg = byCategory[category][player].reduce(function(a, b) {
-          return a + b;
-        }) / byCategory[category][player].length
-        if (playerAverages[player]  === undefined){
-          playerAverages[player] = [avg];
-        } else {
-        playerAverages[player].push(avg)
-        }
-        console.log(playerAverages)
+
+    console.log("req[0].testData", req[0].testData);
+    displayData = {}
+    displayData.labels = getParticipants(testData);
+    displayData.datasets = []
+    for (var j = 0; j < displayData.labels.length; j++) {
+      var feedbackScores = {};
+      feedbackScores[displayData.labels[j]] = [];
+      for (var i = 0; i < testData.length; i++) {
+        feedbackScores[displayData.labels[j]].push(testData[i].feedbacks[displayData.labels[j]])
       }
-      console.log("averages for " + category, playerAverages[category])
+      displayData.datasets[j] = {
+        label: displayData.labels[j],
+        fillColor: "rgba(220,220,220,0.6)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: feedbackScores[displayData.labels[j]]
+      }
     }
-
-  for (var category in playerAverages){
-    for (var player in playerAverages[category])
-      var playerArray.push(playerAverages[category][player])
-    }
-  }
-
-        displayData.datasets.push({
-          labels: category,
-          fillColor: "rgba(220,220,220,0.6)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: playerArray
-      })
 
 
     var recoveredBarChart = new Chart(text).Bar(displayData);
+
+
   });
 }
-
-$(document).ready(function() {
-  getChartData();
-});

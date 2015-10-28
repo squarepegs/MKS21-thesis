5a965af0a6e6d8603cf32f5f05e79faa8c91e997
Brian Boyko <brian.boyko@gmail.com>
2015-10-28 10:47:11 -0500

    Prettification of Student and Index pages

--- client/student/student.js
+++ client/student/student.js
@@ -1,10 +1,85 @@
 // student.js
 
 var socket = io();
+// window.jeopardy is used to locally store question information as well as the state of the buzzer, so that users can't buzz in more than once.
 window.jeopardy = {
   buzzed: false,
   question: {}
 };
+
+// -----------------------
+// REACT COMPONENTS: 
+// -----------------------
+// *Initial Render: <div id="main">
+//   └┬─Main
+//    ├──QA
+//    ├──Buzzer
+//    └──Feedback
+//
+// -----------------------
+
+var Main = React.createClass( {
+  componentDidMount: function () {
+    socket.on( 'student joined', function ( host ) {
+      console.log( "you joined " + host + "'s room" );
+      React.render( <div>
+                      <QA />
+                      <Buzzer />
+                      <Feedback />
+                    </div>, document.getElementById( 'main' ) )
+    } )
+    socket.on( 'disconnect', function () {
+      if ( socket.disconnected === true ) {
+        // Console.logs for adding functionality later. 
+        console.log( window.jeopardy.username + ", you have been disconnected from the game session" )
+      }
+      if ( socket.connected === true ) {
+        console.log( 'someone has disconnected from the server' )
+      }
+    } )
+  },
+  handleClick: function () {
+    window.jeopardy.username = $( '#username' ).val(); // We need to store the username because this field will be removed from the DOM on submit.
+    window.jeopardy.code = $( '#code' ).val().toUpperCase(); // jQuery is sometimes easier to use than manipulation React state/props
+    if ( window.jeopardy.username.length < 1 ) alert( "Please enter a username." );
+    else socket.emit( 'student join', {
+        id: window.jeopardy.username,
+        code: window.jeopardy.code
+      } );
+    React.render( <div>
+                    <QA />
+                    <Buzzer />
+                  </div>, document.getElementById( 'main' ) ) // This basically replaces the entire page. 
+  },
+  render: function () {
+    return (
+    <div className="signin">
+      <h1>Sign in to play:</h1>
+      <label>
+        Username:
+      </label>
+      <input
+         type="text"
+         className="input"
+         placeholder="Choose a Username"
+         id="username" />
+      <label>
+        Code:
+      </label>
+      <input
+         type="text"
+         className="input"
+         placeholder="Enter Your Code"
+         id="code" />
+      <a
+         onClick={ this.handleClick }
+         className="waves-effect waves-light btn-large"><i className="material-icons right">play_arrow</i>Join Game</a>
+      <div id="status"></div>
+    </div>
+    )
+  }
+} )
+
 
 var QA = React.createClass( {
   getInitialState: function () {
@@ -18,16 +93,13 @@ var QA = React.createClass( {
   componentDidMount: function () {
     var context = this;
     socket.on( 'student question', function ( data ) {
+      // Reset the text and color every time there's a question. 
       $( '#buzzer' ).removeClass( "red darken-3" );
       $( '#buzzer' ).text( 'Buzz in!' );
-      context.setState( {
-        category: data.category.toUpperCase()
-      } )
+      context.setState( { category: data.category.toUpperCase() } )
       context.setState( { value: data.value } )
-      context.setState( {
-        question: data.question.toUpperCase()
-      } )
-      context.setState( { buzzed: false } )
+      context.setState( { question: data.question.toUpperCase() } )
+      context.setState( { buzzed: false } ) // reset the buzz state. 
     } )
     socket.on( 'buzz', this.setState( { buzzed: true } ) )
   },
@@ -59,11 +131,12 @@ var Buzzer = React.createClass( {
           <Main />
         </div>,
         document.getElementById( 'main' )
-      )
+      ) // reset the page. 
     } )
 
   },
   clickHandler: function () {
+    // Change the buzzer's color to let student know they have buzzed in.
     $( '#buzzer' ).addClass( "red darken-3" );
     $( '#buzzer' ).text( 'BUZZ!' );
     socket.emit( 'buzz', {
@@ -86,10 +159,9 @@ var Buzzer = React.createClass( {
 
 var Feedback = React.createClass( {
   getInitialState: function () {
+    // On/Off don't ever change, so they might be props. 
     return {
       feedback: 0,
-      on: '/client/images/star_on.png',
-      off: '/client/images/star_off.png'
     }
   },
   componentDidMount: function () {
@@ -121,6 +193,9 @@ var Feedback = React.createClass( {
     this.setFeedback( 5 )
   },
   render: function () {
+    var on = '/client/images/star_on.png';
+    var off = '/client/images/star_off.png';
+    // hmm... can we write this more DRYly? 
     return (
     <div className="container">
       <h4>How difficult was this question?</h4>
@@ -130,38 +205,38 @@ var Feedback = React.createClass( {
         </div>
         <div className="col s2">
           <img
-               name="star1"
-               className="star"
-               onClick={ this.oneStar }
-               src={ this.state.feedback >= 1 ? this.state.on : this.state.off }></img>
+           name="star1"
+           className="star"
+           onClick={ this.oneStar }
+           src={ this.state.feedback >= 1 ? on : off }></img>
         </div>
         <div className="col s2">
           <img
-               name="star2"
-               className="star"
-               onClick={ this.twoStar }
-               src={ this.state.feedback >= 2 ? this.state.on : this.state.off }></img>
+           name="star2"
+           className="star"
+           onClick={ this.twoStar }
+           src={ this.state.feedback >= 2 ? on : off }></img>
         </div>
         <div className="col s2">
           <img
-               name="star3"
-               className="star"
-               onClick={ this.threeStar }
-               src={ this.state.feedback >= 3 ? this.state.on : this.state.off }></img>
+           name="star3"
+           className="star"
+           onClick={ this.threeStar }
+           src={ this.state.feedback >= 3 ? on : off }></img>
         </div>
         <div className="col s2">
           <img
-               name="star4"
-               className="star"
-               onClick={ this.fourStar }
-               src={ this.state.feedback >= 4 ? this.state.on : this.state.off }></img>
+           name="star4"
+           className="star"
+           onClick={ this.fourStar }
+           src={ this.state.feedback >= 4 ? on : off }></img>
         </div>
         <div className="col s2">
           <img
-               name="star5"
-               className="star"
-               onClick={ this.fiveStar }
-               src={ this.state.feedback >= 5 ? this.state.on : this.state.off }></img>
+           name="star5"
+           className="star"
+           onClick={ this.fiveStar }
+           src={ this.state.feedback >= 5 ? on : off }></img>
         </div>
         <div className="col s1">
           More Difficult
@@ -172,70 +247,6 @@ var Feedback = React.createClass( {
   }
 } )
 
-var Main = React.createClass( {
-  componentDidMount: function () {
-    socket.on( 'student joined', function ( host ) {
-      console.log( "you joined " + host + "'s room" );
-      React.render( <div>
-                      <QA />
-                      <Buzzer />
-                      <Feedback />
-                    </div>, document.getElementById( 'main' ) )
-    } )
-
-
-    socket.on( 'disconnect', function () {
-      if ( socket.disconnected === true ) {
-        console.log( window.jeopardy.username + ", you have been disconnected from the game session" )
-      }
-      if ( socket.connected === true ) {
-        console.log( 'someone has disconnected from the server' )
-      }
-    } )
-
-  },
-  handleClick: function () {
-    window.jeopardy.username = $( '#username' ).val();
-    window.jeopardy.code = $( '#code' ).val().toUpperCase();
-    if ( window.jeopardy.username.length < 1 ) alert( "Please enter a username." );
-    else socket.emit( 'student join', {
-        id: window.jeopardy.username,
-        code: window.jeopardy.code
-      } );
-    React.render( <div>
-                    <QA />
-                    <Buzzer />
-                  </div>, document.getElementById( 'main' ) )
-  },
-  render: function () {
-
-    return (
-    <div className="signin">
-      <h1>Sign in to play:</h1>
-      <label>
-        Username:
-      </label>
-      <input
-             type="text"
-             className="input"
-             placeholder="Choose a Username"
-             id="username" />
-      <label>
-        Code:
-      </label>
-      <input
-             type="text"
-             className="input"
-             placeholder="Enter Your Code"
-             id="code" />
-      <a
-         onClick={ this.handleClick }
-         className="waves-effect waves-light btn-large"><i className="material-icons right">play_arrow</i>Join Game</a>
-      <div id="status"></div>
-    </div>
-    )
-  }
-} )
 
 // initial page render
 React.render(

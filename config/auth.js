0c407d260dc3c75754bcb4d48ca6283102b0ce5c
Brian Boyko <brian.boyko@gmail.com>
2016-02-14 14:56:13 -0600

    Fix rookie mistake: uploaded auth.js to github. That's a no-no.

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-// THIS HAS TO BE GIT IGNORED
-// AND CHANGED BEFORE DEPLOYMENT
-module.exports = {
-	'facebookAuth' : {
-		'clientID' : '1677512462535142',
-		'clientSecret': 'f6d58c87f7bae4caada626c7fa2440ce',
-		'callbackURL': 'http://localhost:8000/auth/facebook/callback'
-	}
-}
